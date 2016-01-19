f56ee2abbca3ab839454fb04ff42951ade622b96
Italo Maia <imaia@morphus.com.br>
2016-01-19 17:50:36 -0300

    adicionando instalador

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name='BackGram',
+    version='0.1',
+    author='Victor Pasknel',
+    install_requires=['telepot'],
+    scripts=['backgram.py'],
+)
